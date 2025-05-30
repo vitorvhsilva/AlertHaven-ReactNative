89d012b76bb5b0202e15298668b71e0422556e31
vitorvhsilva <dndragonbr@gmail.com>
2025-05-30 00:17:24 -0300

    Adicionando um círculo base para teste no mapa

--- AlertHaven/src/components/WebMap.tsx
+++ AlertHaven/src/components/WebMap.tsx
@@ -1,19 +1,17 @@
-// components/WebMap.tsx
 import React, { useEffect } from 'react';
-import L from 'leaflet';
+import L, { LatLngExpression } from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 
 const WebMap = () => {
   useEffect(() => {
-    // Inicializa o mapa apenas no navegador
-    const map = L.map('map').setView([-23.5505, -46.6333], 13);
+    const center: LatLngExpression = [-23.5505, -46.6333];
 
-    // Adiciona o tile layer (OpenStreetMap)
+    const map = L.map('map').setView(center, 15);
+
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      attribution: '&copy; OpenStreetMap contributors'
     }).addTo(map);
 
-    // Adiciona um marcador
     const icon = L.icon({
       iconUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png',
       iconRetinaUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png',
@@ -22,13 +20,17 @@ const WebMap = () => {
       iconAnchor: [12, 41],
     });
 
-    L.marker([-23.5505, -46.6333], { icon })
+    L.marker(center, {icon})
       .addTo(map)
-      .bindPopup('Olá, Leaflet no React Native Web!');
+      .bindPopup("Centro do círculo");
+    
+    L.circle(center, {
+      radius: 300, 
+      color: '#3388ff', 
+      fillColor: '#3388ff', 
+      fillOpacity: 0.2 
+    }).addTo(map);
 
-    return () => {
-      map.remove(); // Limpa o mapa ao desmontar o componente
-    };
   }, []);
 
   return <div id="map" style={{ width: '100%', height: '100vh' }} />;
